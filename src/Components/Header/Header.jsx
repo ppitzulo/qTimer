0d70ba292579aa38da95f718c1df853a1e78ac60
Peter Pitzulo <plpitzulo@protonmail.com>
2022-09-22 11:50:15 -0400

    Fix icon not displaying properly

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import Icon from './Icon.svg';
 import './Header.css';
-
 
 const Header = () => {
     return (
         <div class="Header">
-                <img src="images/rubic.svg" alt="Black and white speed cube."/>
+                <img src={Icon} alt="Black and white speed cube."/>
                 <div class="Title">
                     qTimer
                 </div>
